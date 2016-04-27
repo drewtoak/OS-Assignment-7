d36c6c619dd0c3f441f7672fd2a3dac9cac9ddb2
drewtoak <ath50@case.edu>
2016-04-27 16:46:24 -0400

    testing get function in server file

--- rpc_server.c
+++ rpc_server.c
@@ -45,7 +45,7 @@ struct response *get_1_svc(int *argp, struct svc_req *rqstp) {
 		randomindex = generate_num()%100;
 	}
 
-	if (strcmp(messages[randomindex].content, NULL)) {
+	if (sizeof(messages[randomindex].content) == sizeof(NULL)) {
 		result.status_code = -1;
 		strcpy(result.content, "");
 	} else {
